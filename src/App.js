862a8f3bf53476e8072a417405b0a870b661af3e
brelieu05 <88119024+brelieu05@users.noreply.github.com>
2024-01-02 02:14:50 -0800

    Initial Commit & Front-Page Design Finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='overlay'></div>
+      <div className="background">
+          <div className='navbar'>
+            <a href='/' className='Logo'>RateMyClub</a>
+            <div className='accountLinks'>
+              <a href='/' className='SignUp'>Sign Up</a>
+              <a href='/' className='SignIn'>Sign In</a>
+            </div>
+          </div>
+        <div className="Main-Page">
+          <input className='searchBar' placeholder='Type in your club...'/>
+        </div>
+      </div>
     </div>
   );
 }
